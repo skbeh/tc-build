d70b9ac6a3d019d479df0c992171951c69de41d7
skbeh <60107333+skbeh@users.noreply.github.com>
2021-10-07 13:59:18 +0800

    small fix

--- ci/build_agent.sh
+++ ci/build_agent.sh
@@ -7,8 +7,8 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt update
 sudo apt install -y bison ca-certificates ccache clang cmake curl file flex gcc g++ git make ninja-build python3 texinfo zlib1g-dev libssl-dev libelf-dev patchelf
 
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_AUTHOR_NAME"
+git config --global user.email "$GIT_AUTHOR_EMAIL"
 
 # Build a newer version of CMake to satisfy LLVM's requirements
 curl -L https://gitlab.kitware.com/cmake/cmake/-/archive/v3.21.3/cmake-v3.21.3.tar.gz | tar xzf -
