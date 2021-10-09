5ad35b522a3270fdce7742f2c04cc3fa8e6e75c6
skbeh <60107333+skbeh@users.noreply.github.com>
2021-10-09 22:32:49 +0800

    Update build_agent.sh

--- ci/build_agent.sh
+++ ci/build_agent.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update
-sudo apt install -y bison ca-certificates ccache clang cmake curl file flex gcc g++ git make ninja-build python3 texinfo zlib1g-dev libssl-dev libelf-dev patchelf zstd
+sudo apt install -y bison ca-certificates ccache cmake curl file flex gcc g++ git make ninja-build python3 texinfo zlib1g-dev libssl-dev libelf-dev patchelf zstd
 
 git config --global user.name "$GIT_AUTHOR_NAME"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
