39b4ad7d3945783da361bb82082e0977d308eee5
skbeh <60107333+skbeh@users.noreply.github.com>
2021-10-09 22:41:40 +0800

    Update build_agent.sh

--- ci/build_agent.sh
+++ ci/build_agent.sh
@@ -10,14 +10,6 @@ sudo apt install -y bison ca-certificates ccache cmake curl file flex gcc g++ gi
 git config --global user.name "$GIT_AUTHOR_NAME"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
 
-# Build a newer version of CMake to satisfy LLVM's requirements
-curl -L https://gitlab.kitware.com/cmake/cmake/-/archive/v3.21.3/cmake-v3.21.3.tar.gz | tar xzf -
-pushd cmake-v3.21.3
-./bootstrap --parallel="$(nproc)"
-make -j"$(nproc)"
-sudo make install
-popd
-
 # Clone LLVM and apply fixup patches *before* building
 git clone --depth 1 "https://github.com/llvm/llvm-project"
 if [ -n "$(echo patches/*.patch)" ]; then
