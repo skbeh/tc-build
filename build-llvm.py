eef4b3f93b42cf476e986caa53d232cb05d3476a
skbeh <60107333+skbeh@users.noreply.github.com>
2021-10-27 07:28:01 +0800

    Update build-llvm.py

--- build-llvm.py
+++ build-llvm.py
@@ -597,7 +597,7 @@ def fetch_llvm_binutils(root_folder, update, shallow, ref):
         extra_args = ()
         if shallow:
             extra_args = ("--depth", "1")
-            extra_args += ("--single-branch", )
+            extra_args += ("--single-branch", "-b", ref)
         subprocess.run([
             "git", "clone", *extra_args,
             "https://github.com/llvm/llvm-project",
